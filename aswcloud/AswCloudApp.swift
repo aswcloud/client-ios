46dcdd1b6c95aeeaac4b734c48e7a3aa5f89fab3
Piorosen <aoikazto@gmail.com>
2022-02-08 22:18:05 +0900

    프로젝트 커밋

---
+++ aswcloud/AswCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  aswcloudApp.swift
+//  aswcloud
+//
+//  Created by ChaCha on 2022/02/08.
+//
+
+import SwiftUI
+
+@main
+struct AswCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
